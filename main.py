c98cadf3c8c24a1fee245ea2d6f3cd63bfc32e90
unknown <gmargo@mit.edu>
2019-12-07 16:58:01 -0500

    scenario tweaks

--- main.py
+++ main.py
@@ -72,15 +72,15 @@ def three_goals_example():
                             [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
                             [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]])
 
-    goal_locations = [(3, 12), (7, 12), (12, 12)]
+    goal_locations = [(3, 9), (9, 12), (15, 12)]
     goals = ['A','B','C','D','E']
     goal_assignments = {'A': goal_locations[0], 'B': goal_locations[1], 'C': goal_locations[2], 'D': None, 'E': None}
     env = Environment(obstacle_map=obstacle_map, goal_locations=goal_locations, goal_assignments=goal_assignments)
 
 
     # define agents
-    agent1 = Agent(rewards={'A': 3, 'B': 5, 'C': 10, 'D': 5, 'E': 5}, initial_location=(7, 7), initial_beliefs=np.array([[0.5, 0.5], [0.5, 0.5]]), environment=env)
-    agent2 = Agent(rewards={'A': 5, 'B': 8, 'C': 7, 'D': 5, 'E': 5}, initial_location=(11, 7), initial_beliefs=np.array([[0.5, 0.5], [0.5, 0.5]]), environment=env)
+    agent1 = Agent(name="Agent1", rewards={'A': 3, 'B': 5, 'C': 10, 'D': 5, 'E': 5}, initial_location=(9, 7), initial_beliefs=np.array([[0.5, 0.5], [0.5, 0.5]]), environment=env)
+    agent2 = Agent(name="Agent2", rewards={'A': 5, 'B': 8, 'C': 7, 'D': 5, 'E': 5}, initial_location=(15, 7), initial_beliefs=np.array([[0.5, 0.5], [0.5, 0.5]]), environment=env)
 
     #agent1.plan.set_next_location((4, 7))
     #agent2.plan.set_next_location((10, 7))
